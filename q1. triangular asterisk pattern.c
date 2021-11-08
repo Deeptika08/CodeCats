efe9273d2cfd008215fd094cdb86360cc791a236
Deeptika08 <60597673+Deeptika08@users.noreply.github.com>
2021-11-08 17:22:28 +0530

    Add files via upload
    week 3 update

---
+++ q1. triangular asterisk pattern.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,j;
+	for(i=01; i<=5;i++) /* just moves the loop and controls new line  */
+	{
+	for(j=01;j<=i;j++) /* prints stars  */
+	{
+	printf("*");
+	}
+	printf("\n");
+	}
+
+return 0;
+}
+
